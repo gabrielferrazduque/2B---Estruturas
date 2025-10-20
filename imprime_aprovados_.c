6166bd8dc4feadcd9d763e9a2db7d9068474667e
Gabriel Ferraz Duque <83476335+gabrielferrazduque@users.noreply.github.com>
2025-10-20 16:12:54 -0300

    Add files via upload

---
+++ imprime_aprovados_.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct {
+    char matricula[8];
+    char nome[81];
+    char turma;
+    float notas[3];
+} Aluno;
+
+void imprime_aprovados(int n, Aluno **turmas) {
+    printf("\n=== Alunos Aprovados ===\n");
+    for (int i = 0; i < n; i++) {
+        float media = (turmas[i]->notas[0] + turmas[i]->notas[1] + turmas[i]->notas[2]) / 3.0;
+        if (media >= 7.0) {
+            printf("Matrícula: %s\n", turmas[i]->matricula);
+            printf("Nome: %s\n", turmas[i]->nome);
+            printf("Turma: %c\n", turmas[i]->turma);
+            printf("Média: %.2f\n", media);
+            printf("------------------------\n");
+        }
+    }
+}
+imprime_aprovados(5, alunos);
+{} 
+
+return 0; 
